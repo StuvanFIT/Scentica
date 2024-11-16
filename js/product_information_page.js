8cba7a1e59874812971d2ecab2bba6318362366b
skai0008 <skai0008@student.monash.edu>
2024-11-16 18:16:14 +1100

    Adds latest changes

--- js/product_information_page.js
+++ js/product_information_page.js
@@ -9,43 +9,64 @@ export function displayProductInfo(){
 
         console.log(currentProduct)
 
+        //Append images to the art gallery:
+        displayImageGallery(currentProduct);
 
-        const currGallery = document.querySelector(".galleryContainer");
-        
-        const image1 = document.createElement("img");
-        image1.src= currentProduct.images.image1;
-        image1.alt = currentProduct.name;
-        image1.className = "gallery-image";
-        
-        const image2 = document.createElement("img");
-        image2.src= currentProduct.images.image2;
-        image2.alt = currentProduct.name;
-        image2.className = "gallery-image";
-
-        const image3 = document.createElement("img");
-        image3.src= currentProduct.images.image3;
-        image3.alt = currentProduct.name;
-        image3.className = "gallery-image";
-
-        const image4 = document.createElement("img");
-        image4.src= currentProduct.images.image4;
-        image4.alt = currentProduct.name;
-        image4.className = "gallery-image";
+        //Append product information to the details section:
+        displayProdDetails(currentProduct);
 
 
-        currGallery.appendChild(image1);
-        currGallery.appendChild(image2);
-        currGallery.appendChild(image3);
-        currGallery.appendChild(image4);
 
-        
-        
 
-        
 
-        
+
+
     }
 }
+
+function displayImageGallery(currentProduct){
+    
+    const currGallery = document.querySelector(".galleryContainer");
+        
+    const image1 = document.createElement("img");
+    image1.src= currentProduct.images.image1;
+    image1.alt = currentProduct.name;
+    image1.className = "gallery-image";
+    
+    const image2 = document.createElement("img");
+    image2.src= currentProduct.images.image2;
+    image2.alt = currentProduct.name;
+    image2.className = "gallery-image";
+
+    const image3 = document.createElement("img");
+    image3.src= currentProduct.images.image3;
+    image3.alt = currentProduct.name;
+    image3.className = "gallery-image";
+
+    const image4 = document.createElement("img");
+    image4.src= currentProduct.images.image4;
+    image4.alt = currentProduct.name;
+    image4.className = "gallery-image";
+
+
+    currGallery.appendChild(image1);
+    currGallery.appendChild(image2);
+    currGallery.appendChild(image3);
+    currGallery.appendChild(image4);
+}
+
+function displayProdDetails(currentProduct){
+
+    const prodTitle = document.getElementById("prodTitle");
+    console.log(prodTitle)
+    prodTitle.textContent = currentProduct.name;
+
+    
+}
+
+
+
+
 
 
 displayProductInfo();
